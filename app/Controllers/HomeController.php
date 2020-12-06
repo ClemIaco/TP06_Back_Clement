6d1286c7c5bb078e3c63a09cf12a6b6ad43bc89e
Clement IACOBUCCI <clement.iacobucci@gmail.com>
2020-12-06 13:54:12 +0100

    debug args

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,8 +9,7 @@ class HomeController {
     
     public function home(Request $request, Response $response, array $args)
     {
-        $response->getBody()->write($res);
-        $res = print_r($args);
+        $response->getBody()->write("Page d'accueil");
         return $response;
     }
 }
